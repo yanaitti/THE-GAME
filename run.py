2c2dad326e13f94d3a7049e85a82eeb52cff06cf
Norihisa Yanai <yanaitti@gmail.com>
2020-05-24 19:33:54 +0900

    update

--- run.py
+++ run.py
@@ -219,4 +219,5 @@ def edit_profile(gameid, clientid, nickname):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000, threaded=True)
+    app.run(debug=True)
+#    app.run(debug=True, port=5000, threaded=True)
